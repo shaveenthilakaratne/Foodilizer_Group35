af35cad5b5fcbcf54f0690f55f2f0284e5b0bea7
Shaveen Thilakaratne <65050381+shaveenthilakaratne@users.noreply.github.com>
2021-08-03 23:10:45 +0530

    CustomerProfile_UI

---
+++ Controllers/Customer_profileController.cs
@@ -0,0 +1,32 @@
+﻿using Foodilizer_Group35.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foodilizer_Group35.Controllers
+{
+    public class Customer_profileController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public Customer_profileController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult customer_profile()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
